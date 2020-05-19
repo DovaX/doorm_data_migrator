fc6b45c154d1e5c6ea200d5a2b000bff2d505a94
Dominik Vach <vach.dominik@gmail.com>
2020-05-19 19:39:44 +0200

    dpf import

--- doorm_data_migrator.py
+++ doorm_data_migrator.py
@@ -1,5 +1,7 @@
 import dorm as dm
-import dogui as dg
+import dogui.dogui_core as dg
+import pickle
+import dorm_pandas_framework as dpf
 
 
 
@@ -8,6 +10,11 @@ db1 = dm.db("config1.ini")
 db2 = dm.Mysqldb("config2.ini")
 
 gui1=dg.GUI()
+
+list_of_table_names=['companies']
+dorm_tables=dpf.init_dorm_tables(db1,list_of_table_names)
+dfs=dpf.init_dataframes_from_tables(db1,dorm_tables,list_of_table_names)
+print(dfs)
 
 
 gui1.build_gui()
