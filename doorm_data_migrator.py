e541f645af66fba29d5d9171ff00ed8b919b1c0c
Dominik Vach <vach.dominik@gmail.com>
2020-06-04 23:22:04 +0200

    migrate table content

--- doorm_data_migrator.py
+++ doorm_data_migrator.py
@@ -2,17 +2,33 @@ import dorm as dm
 import dogui.dogui_core as dg
 import pickle
 import dorm_pandas_framework as dpf
+import pandas as pd
+db1 = dm.db("config-shipvio.ini")
+
+ds_free_carriers_table=dm.Table.init_all_columns(db1,"ds_free_carriers")
+
+
+rows=ds_free_carriers_table.select("SELECT top(10) * FROM dbo.ds_free_carriers")
+
+ds_free_carriers_df=pd.DataFrame(rows,columns=ds_free_carriers_table.columns)
+ds_free_carriers_df.drop(["id"],axis=1,inplace=True)
+
+db1.close_connection()
+
+print(ds_free_carriers_df.columns)
+db2 = dm.Mysqldb("config-mysql-hot-mode.ini")
+
+
+ds_free_demands_table_target=dm.Table.init_all_columns(db2,"ds_free_carriers")
+
+print(ds_free_demands_table_target.columns)
+
+ds_free_demands_table_target.insert_from_df(ds_free_carriers_df)
 
 
 
-db1 = dm.db("config1.ini")
 
-db2 = dm.Mysqldb("config2.ini")
-
-list_of_table_names=['companies']
-dorm_tables=dpf.init_dorm_tables(db1,list_of_table_names)
-dfs=dpf.init_dataframes_from_tables(db1,dorm_tables,list_of_table_names)
-print(dfs)
+db2.close_connection()
 
 
 def check_tables(dfs):
@@ -26,7 +42,7 @@ def insert_data_to_db(dfs,target_db):
     insert_data(dfs,target_db)
     
     
-
+    
 #gui1=dg.GUI()
 
 #gui1.build_gui()
